e1db9877c83119493b1904fefef7f28566f9f8d3
Mitron57 <job.zakharyan@gmail.com>
2025-05-03 23:45:57 +0300

    fix: prevent goroutine leakage

--- bus.go
+++ bus.go
@@ -1,137 +1,140 @@
 package subPub
 
 import (
-    "context"
-    "sync"
-    "sync/atomic"
+	"context"
+	"sync"
+	"sync/atomic"
 )
 
 // MessageHandler is a callback function that processes messages delivered to subscribers.
 type MessageHandler func(msg any)
 
 type SubPub interface {
-    // Subscribe creates an asynchronous queue subscriber on the given object.
-    Subscribe(subject string, handler MessageHandler) (Subscription, error)
-    // Publish publishes the msg argument to the given subject.
-    Publish(subject string, msg any) error
-    // Close will shut down sub-pub system.
-    // May be blocked by data delivery until the context is cancelled.
-    Close(ctx context.Context) error
+	// Subscribe creates an asynchronous queue subscriber on the given object.
+	Subscribe(subject string, handler MessageHandler) (Subscription, error)
+	// Publish publishes the msg argument to the given subject.
+	Publish(subject string, msg any) error
+	// Close will shut down sub-pub system.
+	// May be blocked by data delivery until the context is cancelled.
+	Close(ctx context.Context) error
 }
 
 type bus struct {
-    mx        sync.RWMutex
-    topics    map[string]*subject
-    listener  chan string
-    closeChan chan struct{}
-    closed    atomic.Bool
+	mx        sync.RWMutex
+	topics    map[string]*subject
+	listener  chan string
+	closeChan chan struct{}
+	closed    atomic.Bool
 }
 
 func NewPubSub() SubPub {
-    return &bus{
-        topics:    make(map[string]*subject),
-        listener:  make(chan string),
-        closeChan: make(chan struct{}),
-    }
+	return &bus{
+		topics:    make(map[string]*subject),
+		listener:  make(chan string),
+		closeChan: make(chan struct{}),
+	}
 }
 
 // Detaches subscriber from bus.
 func (b *bus) detach(subject string, subscriptionId int) {
-    b.mx.RLock()
-    topic, ok := b.topics[subject]
-    b.mx.RUnlock()
+	b.mx.RLock()
+	topic, ok := b.topics[subject]
+	b.mx.RUnlock()
 
-    if !ok {
-        return
-    }
+	if !ok {
+		return
+	}
 
-    topic.remove(subscriptionId)
+	topic.remove(subscriptionId)
 }
 
 // Deletes subject from topics
 func (b *bus) closeSubject(subject string) {
-    b.mx.Lock()
-    defer b.mx.Unlock()
-    delete(b.topics, subject)
+	b.mx.Lock()
+	defer b.mx.Unlock()
+	delete(b.topics, subject)
 }
 
 // Subscribe registers new subscriber to the subject.
 // If there's no such subject, then the new topic will be created.
 // ErrNilHandler and ErrClosedBus are returned respectively to their reason.
 func (b *bus) Subscribe(subject string, handler MessageHandler) (Subscription, error) {
-    if handler == nil {
-        return nil, ErrNilHandler
-    }
+	if handler == nil {
+		return nil, ErrNilHandler
+	}
 
-    if b.closed.Load() {
-        return nil, ErrClosedBus
-    }
+	if b.closed.Load() {
+		return nil, ErrClosedBus
+	}
 
-    b.mx.RLock()
-    topic, ok := b.topics[subject]
-    b.mx.RUnlock()
+	b.mx.RLock()
+	topic, ok := b.topics[subject]
+	b.mx.RUnlock()
 
-    if !ok {
-        topic = newTopic(subject, b.listener, b.closeChan)
-        b.mx.Lock()
-        b.topics[subject] = topic
-        b.mx.Unlock()
-        go topic.listen()
-    }
+	if !ok {
+		topic = newTopic(subject, b.listener, b.closeChan)
+		b.mx.Lock()
+		b.topics[subject] = topic
+		b.mx.Unlock()
+		go topic.listen()
+	}
 
-    id := topic.add(handler)
-    return newSubscription(b, subject, id), nil
+	id := topic.add(handler)
+	return newSubscription(b, subject, id), nil
 }
 
 // Publish method asynchronously delivers message to the subject's handlers
-// ErrNoSuchSubject is returned, when there's no such subject (interesting, isn't it?), 
+// ErrNoSuchSubject is returned, when there's no such subject (interesting, isn't it?),
 // ErrClosedBus is returned when there's attempt to publish on a closed (or closing) bus.
 func (b *bus) Publish(subject string, msg any) error {
-    if b.closed.Load() {
-        return ErrClosedBus
-    }
+	if b.closed.Load() {
+		return ErrClosedBus
+	}
 
-    b.mx.RLock()
-    topic, ok := b.topics[subject]
-    b.mx.RUnlock()
+	b.mx.RLock()
+	topic, ok := b.topics[subject]
+	b.mx.RUnlock()
 
-    if !ok {
-        return ErrNoSuchSubject
-    }
+	if !ok {
+		return ErrNoSuchSubject
+	}
 
-    topic.messageQueue <- msg
-    return nil
+	topic.messageQueue <- msg
+	return nil
 }
 
 // Close performs bus closing. Topics are closed in undefined order.
-// It may be cancelled via context, but be aware that the bus is in closed state during closing.
+// It may be cancelled via context, but be aware that the bus is in closed state during closing
+// and closed topics are not restored.
 func (b *bus) Close(ctx context.Context) error {
-    if !b.closed.CompareAndSwap(false, true) {
-        return ErrClosedBus
-    }
+	if !b.closed.CompareAndSwap(false, true) {
+		return ErrClosedBus
+	}
 
-    topicsToClose := len(b.topics)
+	topicsToClose := len(b.topics)
 
-    if topicsToClose == 0 {
-        close(b.listener)
-        close(b.closeChan)
-        return nil
-    }
+	if topicsToClose == 0 {
+		close(b.listener)
+		close(b.closeChan)
+		return nil
+	}
 
-    for closedTopics := 0; closedTopics < topicsToClose; {
-        b.closeChan <- struct{}{}
+	for closedTopics := 0; closedTopics < topicsToClose; {
+		b.closeChan <- struct{}{}
+		closedTopics++
 
-        select {
-        case <-ctx.Done():
-            b.closed.Store(false)
-            return ctx.Err()
-        case topic := <-b.listener:
-            b.closeSubject(topic)
-            closedTopics++
-        }
-    }
+		select {
+		case <-ctx.Done():
+			topic := <-b.listener
+			b.closeSubject(topic)
+			b.closed.Store(false)
+			return ctx.Err()
+		case topic := <-b.listener:
+			b.closeSubject(topic)
+		}
+	}
 
-    close(b.listener)
-    close(b.closeChan)
-    return nil
+	close(b.listener)
+	close(b.closeChan)
+	return nil
 }
